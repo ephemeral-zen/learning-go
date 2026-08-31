e92b47a157c979e7ee08c42703298f380d0589c4
Anastasiia <98601803+kaanastasiia@users.noreply.github.com>
2026-08-31 23:14:57 +0300

    implement logging functions

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -1,30 +1,49 @@
 package pocketlog
 
+import (
+	"fmt"
+)
+
 type Logger struct {
 	threshold Level
 }
 
 func (l *Logger) Debugf(format string, args ...any) {
-
+	if l.threshold > LevelDebug {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 func (l *Logger) Infof(format string, args ...any) {
-
+	if l.threshold > LevelInfo {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 func (l *Logger) Warnf(format string, args ...any) {
-
+	if l.threshold > LevelWarn {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
-
+	if l.threshold > LevelError {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 func (l *Logger) Fatalf(format string, args ...any) {
-
+	if l.threshold > LevelFatal {
+		return
+	}
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
-// Create a new instance of the Logger struct
+// Create a new instance of the Logger struct and return its address in memory
 func New(threshold Level) *Logger {
 	return &Logger{
 		threshold: threshold,
